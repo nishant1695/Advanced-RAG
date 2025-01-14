dc50164925a4bbd257438e4d6df1d954de32bdf0
Nishant Singh <nishant1695@gmail.com>
2025-01-14 15:33:11 -0500

    Updated prompt

--- Query_Decomposition_RAG.py
+++ Query_Decomposition_RAG.py
@@ -60,10 +60,6 @@ def setup_ollama_language_model_chain(vectorstore: Chroma, LLM_name: str, topk: 
                 {context}
 
                 Question: {question}
-
-                Start with a brief introduction to the topic, addressing the key elements of the question. Then, proceed with a detailed analysis, breaking down each component of the question into separate, well-thought-out points. Ensure that each point is supported by logical reasoning and relates back to the context provided. Conclude with a summary that synthesizes the findings and reflects on the implications or outcomes.
-
-                Let's work this out step by step to ensure a thorough and well-structured answer.
                 """
     prompt = ChatPromptTemplate.from_template(template)
 
@@ -137,10 +133,6 @@ def setup_language_model_chain(vectorstore: Chroma, topk: int):
                 {context}
 
                 Question: {question}
-
-                Start with a brief introduction to the topic, addressing the key elements of the question. Then, proceed with a detailed analysis, breaking down each component of the question into separate, well-thought-out points. Ensure that each point is supported by logical reasoning and relates back to the context provided. Conclude with a summary that synthesizes the findings and reflects on the implications or outcomes.
-
-                Let's work this out step by step to ensure a thorough and well-structured answer.
                 """
     prompt = ChatPromptTemplate.from_template(template)
 
